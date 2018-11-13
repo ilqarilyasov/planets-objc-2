b0428ae7034b0bce5f2bf2166379a132b59e4c37
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-13 15:55:42 -0600

    Initial Commit

---
+++ Planets Objc2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Planets Objc2
+//
+//  Created by Ilgar Ilyasov on 11/13/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
